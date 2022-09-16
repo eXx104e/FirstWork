d5055a7d4a2539602b6f108517b13556c548f5d4
eXx104e <oxytocaexxidae@gmail.com>
2022-09-16 19:21:23 +0300

    1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        /*Console.WriteLine("Задание №1 ");
+        Console.WriteLine(Math.Round(Math.E, 1));
+        Console.WriteLine();
+        Console.ReadKey();
+
+        Console.WriteLine("Задание №2");
+        Console.WriteLine("50");
+        Console.WriteLine("10");
+        Console.WriteLine();
+        Console.ReadKey();
+
+        Console.WriteLine("Задание №3");
+        Console.WriteLine("11\r\n121\r\n1331\r\n14641");
+        Console.WriteLine();
+        Console.ReadKey();
+
+        Console.WriteLine("Задание №4");
+        Console.WriteLine("Введите любое число");
+        double x = double.Parse(Console.ReadLine());
+        double x1 = x + 10;
+        Console.WriteLine(x1);
+        Console.WriteLine();
+        Console.ReadKey(); 
+
+        Console.WriteLine("Задание №5");
+        Console.WriteLine("Введите длину стороны квадрата");
+        double y = double.Parse(Console.ReadLine());
+        double y1 = y * 4;
+        Console.WriteLine(x1);
+        Console.WriteLine();
+        Console.ReadKey();
+
+        Console.WriteLine("Задание №6");
+        Console.WriteLine("Введите длину радиуса круга");
+        double c = double.Parse(Console.ReadLine());
+        double c1 = c * c * Math.PI;
+        double c2 = 2 * c * Math.PI;
+        Console.WriteLine($"Площадь круга равна: {c1}");
+        Console.WriteLine($"Длина окружности равна: {c2}");
+        Console.WriteLine();
+        Console.ReadKey();
+
+        Console.WriteLine("Задание №7");
+        Console.WriteLine("Введите аргумент х для функции y=cos(x)");
+        double f = double.Parse(Console.ReadLine());
+        double f1 = Math.Cos(f);
+        Console.WriteLine(f1);
+        Console.WriteLine();
+        Console.ReadKey(); */
+
+        Console.WriteLine("Задание №8");
+        Console.WriteLine("Введите длину высоты трапеции");
+        double length = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите длину верхнего основания трапеции");
+        double upperlength = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите длину нижнего основания трапеции");
+        double lowerlength = double.Parse(Console.ReadLine());
+        double u1 = Math.Sqrt(length * length + Math.Abs((upperlength - lowerlength) / 2) * Math.Abs((upperlength - lowerlength) / 2));
+        double resultat = upperlength + lowerlength + 2 * u1;
+        Console.WriteLine($"Периметр трапеции равен: {resultat}");
+        Console.WriteLine();
+        Console.ReadKey();
+
+
+    }
+}
--- Tumakov.labs/Program.cs
+++ Tumakov.labs/Program.cs
@@ -1,1 +1,77 @@
-﻿
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Упражнение 2.1");
+        Console.WriteLine("Введите свое имя");
+        string name = (Console.ReadLine());
+        Console.WriteLine($"Привет, {name}");
+        Console.WriteLine("");
+
+        Console.WriteLine("Упражнение 2.2");
+        Console.WriteLine("Введите целое число");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите еще одно целое число");
+        int b = int.Parse(Console.ReadLine());
+        if (b == 0)
+        {
+            Console.WriteLine("Введите новое целое число, отличное от нуля");
+            int c = int.Parse(Console.ReadLine());
+            int res = a / c;
+            Console.WriteLine(res);
+        }
+        else
+        {
+            int res = a / b;
+            Console.WriteLine(res);
+        }
+        Console.WriteLine("");
+
+        Console.WriteLine("Домашнее задание 2.1");
+        Console.WriteLine("Введите букву");
+        string letter = Console.ReadLine();
+        int index = char.Parse(letter);
+        int index1 = index + 1;
+        Console.WriteLine((char)index1);
+        Console.WriteLine();
+
+        Console.WriteLine("Домашнее заданите 2.2");
+        Console.WriteLine("Введите первый коэффициент квадратного уравнения");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второй коэффициент квадратного уравнения");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите третий коэффициент квадратного уравнения");
+        int c = int.Parse(Console.ReadLine());
+        int discriminant = b * b - 4 * a * c; 
+        if (discriminant < 0)
+        {
+            Console.WriteLine("Нет корней, т.к. D < 0");
+        }
+        if (discriminant == 0)
+        {
+            double x1 = -b / (2 * a);
+            Console.WriteLine($"Уравнение имеет один корень: {x1}");
+        }
+        if (discriminant > 0)
+        {
+            double x2 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+            double x3 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+            Console.WriteLine($"Уравнение имеет корни: {x2},{x3}");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
